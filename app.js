cb32226eb1fe41f97495861f756ea05e6e942311
aditya shrivastava <aditya.shrivastava@aerchain.io>
2021-03-05 00:39:07 +0530

    added email service for info to team

--- app.js
+++ app.js
@@ -44,10 +44,6 @@ db.on("error", function (err) {
 // 	}
 // });
 
-app.get("/", function (req, res) {
-	res.send("Hello World");
-});
-
 app.get("/favicon.ico", (req, res) => res.status(204));
 
 app.use(cors());
@@ -61,6 +57,21 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 // Routes
+
+app.all('*', (req, res, next) => {
+    console.log('Before processing - ', req.url, req.method);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0');
+    res.header('Strict-Transport-Security', 'maxAge=100000');
+    res.header('X-Frame-Options', 'DENY');
+    res.header('Content-Security-Policy', "frame-ancestors 'none'");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Cookies, cookies, x-access-token, Origin, Content-Type, Accept');
+    return next();
+  });
+app.get("/", function (req, res) {
+	res.send("Hello World");
+});
 app.use("/api/files", require("./routes/files"));
 app.use("/files", require("./routes/show"));
 app.use("/files/download", require("./routes/download"));
